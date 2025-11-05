8dc0705adcffd2f6e5373d69721b74aed29c02a0
Puntawat Subhamani <puntawatsub@gmail.com>
2025-11-05 14:31:15 +0200

    iteration 6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,51 @@
+import logo from "../assets/images/logo.svg";
+import PageLinks from "./PageLinks";
+
 const Navbar = () => {
   return (
-    <div>
-      <p className="section-dummy">About</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars" />
+          </button>
+        </div>
+        <PageLinks parentClass="nav-links" itemClass="nav-link" />
+        <ul className="nav-icons">
+          <li>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noreferrer"
+              className="nav-icon"
+            >
+              <i className="fab fa-facebook" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.x.com"
+              target="_blank"
+              rel="noreferrer"
+              className="nav-icon"
+            >
+              <i className="fab fa-x-twitter" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.squarespace.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="nav-icon"
+            >
+              <i className="fab fa-squarespace" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,9 +1,25 @@
+import tour1 from "../assets/images/tour-1.jpeg";
+import tour2 from "../assets/images/tour-2.jpeg";
+import tour3 from "../assets/images/tour-3.jpeg";
+import tour4 from "../assets/images/tour-4.jpeg";
+import Tour from "./Tour";
+
+import { tours } from "../data";
+
 function Tours() {
   return (
-    <div>
-      <p className="section-dummy">Tours</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <section className="section" id="tours">
+      <div className="section-title">
+        <h2>
+          featured <span>tours</span>
+        </h2>
+      </div>
+      <div className="section-center featured-center">
+        {tours.map((tour) => (
+          <Tour key={tour.id} {...tour}></Tour>
+        ))}
+      </div>
+    </section>
   );
 }
 
